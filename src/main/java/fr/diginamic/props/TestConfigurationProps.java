2ce97ef1da84945a310e7c72ee16f4120c8d30e1
Cherhazad <cherhazad.boustani@outlook.fr>
2024-05-30 10:50:43 +0200

    TP Fichiers Configuration - Partie 1

---
+++ src/main/java/fr/diginamic/props/TestConfigurationProps.java
@@ -0,0 +1,13 @@
+package fr.diginamic.props;
+
+import java.util.ResourceBundle;
+
+public class TestConfigurationProps {
+
+	public static void main(String[] args) {
+		ResourceBundle config = ResourceBundle.getBundle("config");
+		String url = config.getString("database.url");
+		System.out.println(url);
+	}
+
+}
